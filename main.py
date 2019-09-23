5e831d73c3d5fab38792357e17d17d44fd7cf825
Rolly Maulana Awangga <rolly@awang.ga>
2019-09-23 10:56:41 +0700

    ok

--- main.py
+++ main.py
@@ -8,16 +8,20 @@ Created on Sat Sep 21 20:54:32 2019
 
 import kepo
 import qrcode
+import requests
 
-
+cdn="https://raw.githubusercontent.com/D4TI/2018/master/kecil/"
 
 
 kepo = kepo.Kepo()
 print("NPM : ")
 NPM=input()
 
-crot=kepo.generateURL(NPM)
-
-img = qrcode.make(crot)
-img.save(NPM+".jpg")
-print("File QrCode Telah Dibuat")
+f = requests.get(cdn+NPM+'.jpg')
+if f.text[:3] == '404':
+	print("Upload Foto Dulu Coy")
+else:
+	crot=kepo.generateURL(NPM)
+	img = qrcode.make(crot)
+	img.save(NPM+".png")
+	print("File QrCode Telah Dibuat")
